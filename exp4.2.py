5cc3b16aa78b9a5faccaa57fa7985b1256bcb970
ltceuzr <ur7900045@gmail.com>
2026-04-17 21:46:55 +0530

    Update exp4.2.py

--- exp4.2.py
+++ exp4.2.py
@@ -12,3 +12,18 @@ def main():
 
 if __name__ == "__main__":
 	main()
+
+# 1. Install pip
+> sudo apt install python3-pip
+
+# 2. Install virtual environment support
+> sudo apt install python3-venv
+
+# 3. Install PyInstaller using pip
+> python3 -m pip install pyinstaller
+
+
+> python3 -m PyInstaller --onefile main.py
+
+> cd dist
+> ./g1
